27b548495ea14cd993fde74e04ea29bf4b4955a2
Brandon R <brandon.p.r@hotmail.com>
2022-03-14 00:31:14 -0400

    Reverting MakeConn/Do funcs.

--- client.go
+++ client.go
@@ -1,40 +1,6 @@
 package lzhttp
 
-// This is a temp build for tests.
-func (Data *Client) MakeConn(method, addr string, config ReqConfig) (err error) {
-	if config.Proxy != nil {
-		if err = Data.GrabUrl(addr, method).ConnectProxy(config); err != nil {
-			return err
-		}
-	} else {
-		if err = Data.GrabUrl(addr, method).GenerateConn(config); err != nil {
-			return err
-		}
-	}
-
-	if len(config.Cookies) != 0 {
-		Data.Config.Headers["cookie"] += config.Cookies
-	}
-
-	Data.WriteSettings()
-	Data.Windows_Update()
-	Data.Send_Prio_Frames()
-	Data.reqclient = config
-
-	return
-}
-
-// This is a temp build for tests.
-func (Data *Client) Do() (Res Response, err error) {
-	Data.SendSettings(Data.Config.Headers[":method"])
-
-	if Data.Config.Headers[":method"] != "GET" {
-		Data.Client.DataSend(Data.reqclient.Data)
-	}
-
-	return Data.FindData(Data.reqclient)
-}
-
+// Handles conns and sending of requests, GO doesnt allow multiple reqs to be sent through the framer.
 func (Data *Client) DefaultRequest(method, addr string, config ReqConfig) (Res Response, err error) {
 	if config.Proxy != nil {
 		if err = Data.GrabUrl(addr, method).ConnectProxy(config); err != nil {
