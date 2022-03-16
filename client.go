b4e97fd6d1b639d35a505e91d64a97b88152c72b
Brandon R <brandon.p.r@hotmail.com>
2022-03-16 16:23:07 -0400

    DefaultRequest api updates

--- client.go
+++ client.go
@@ -1,15 +1,13 @@
 package lzhttp
 
 // Handles conns and sending of requests, GO doesnt allow multiple reqs to be sent through the framer.
-func (Data *Client) DefaultRequest(method, addr string, config *ReqConfig) (Res Response, err error) {
+func (Data *Client) DefaultRequest(method, addr string, config ReqConfig) (Res Response, err error) {
 	if config.Proxy != nil {
-		if err = Data.GrabUrl(addr, method).ConnectProxy(*config); err != nil {
+		if err := Data.GrabUrl(addr, method).ConnectProxy(config); err != nil {
 			return Response{}, err
 		}
-	} else {
-		if err = Data.GrabUrl(addr, method).GenerateConn(*config); err != nil {
-			return Response{}, err
-		}
+	} else if err := Data.GrabUrl(addr, method).GenerateConn(config); err != nil {
+		return Response{}, err
 	}
 
 	if len(config.Cookies) != 0 {
@@ -22,7 +20,7 @@ func (Data *Client) DefaultRequest(method, addr string, config *ReqConfig) (Res 
 		Data.Client.DataSend(config.Data)
 	}
 
-	return Data.FindData(*config)
+	return Data.FindData(config)
 }
 
 func (Data *Client) DeleteHeader(headernames ...string) {
